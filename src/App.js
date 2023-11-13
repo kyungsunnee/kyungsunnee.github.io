05c28bd3436d0cfdc36b01ad173d3c0b463519ba
kyungsunnee <83901698+kyungsunnee@users.noreply.github.com>
2023-11-13 23:44:19 +0900

    script update project10

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import mostico from "./img/skill/bing.png";
 import linkedico from "./img/link.png";
 import favico from "./img/favourite2.png";
 import leftquotesico from "./img/left-quotes-sign.png";
+import keywordico from "./img/check (1).png";
 
 function App() {
   const icoList = [
@@ -83,8 +84,17 @@ function App() {
               <div className="script-title-sq"></div>
               <div className="script-title-txt">ABOUT</div>
             </div>
-            <div className="script-txt">
-              <strong>커뮤니케이션</strong>
+            <div className="script-txt color-gray">
+              <div id="goal-script">
+                “ 서비스를 통한 사용자와의 원활한 의사소통은 물론, 팀 내에서의
+                긍정적인 커뮤니케이션을 통해 협업의 효율성을 극대화하고자 하는
+                목표를 가지고 있습니다. 사용자 경험 개선 및 팀 내 협업을
+                강화하여 효율적이고 창의적인 결과물을 만들어내고 싶습니다. ”
+              </div>
+              <br />
+              <br />
+              <img src={keywordico} className="keyword-ico" />
+              <strong className="keyword-title">커뮤니케이션</strong>
               <br />
               사용자와의 원활한 의사소통을 핵심가치로 삼습니다. 경험을 통해 모든
               사용자에게 편안하고 유쾌한 경험을 제공하는 것에 목표를 두며, 항상
@@ -93,7 +103,8 @@ function App() {
               합니다.
               <br />
               <br />
-              <strong>용감함</strong>
+              <img src={keywordico} className="keyword-ico" />
+              <strong className="keyword-title">용감함</strong>
               <br />
               처음 사용하는 툴에 대한 두려움이 없으며, 높은 적응력과 학습 능력을
               가지고 있습니다. 이전에 경험하지 못한 업무가 주어져도 가지고있는
@@ -102,7 +113,8 @@ function App() {
               해결책을 제시하는 데 도움이 될 것입니다.
               <br />
               <br />
-              <strong>꾸준함</strong>
+              <img src={keywordico} className="keyword-ico" />
+              <strong className="keyword-title">꾸준함</strong>
               <br />
               꾸준한 학습 의지는 최신 트렌드를 파악하고 빠르게 적응하는 데 큰
               역할을 합니다. 업계의 동향을 주시하며 끊임없이 학습하고, 새로운
@@ -110,11 +122,12 @@ function App() {
               통해 기업의 현대적인 모습을 구축하는 데 도움이 될 것입니다.
               <br />
               <br />
-              <strong>끈질김</strong>
+              <img src={keywordico} className="keyword-ico" />
+              <strong className="keyword-title">끈질김</strong>
               <br />
               끈질김은 뛰어난 결과물을 이끌어내는 데 결정적인 역할을 합니다.
               마지막까지 수정 및 보완 작업을 통해 최고의 결과를 창출합니다.
-              <br />
+              {/* <br />
               <br />
               <br />
               <div id="goal-script">
@@ -122,7 +135,7 @@ function App() {
                 긍정적인 커뮤니케이션을 통해 협업의 효율성을 극대화하고자 하는
                 목표를 가지고 있습니다. 사용자 경험 개선 및 팀 내 협업을
                 강화하여 효율적이고 창의적인 결과물을 만들어내고 싶습니다. "
-              </div>
+              </div> */}
             </div>
 
             <div className="empty_space"></div>
